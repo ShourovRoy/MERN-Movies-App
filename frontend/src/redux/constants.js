638fa1122f56fd1cb71e6da3bfd3a416c5148aba
Shourov Roy <shourovroy2389@gmail.com>
2024-05-05 17:58:04 +0600

    Dockerfile and compose

---
+++ frontend/src/redux/constants.js
@@ -0,0 +1,5 @@
+export const BASE_URL = "";
+export const USERS_URL = "http://localhost:3000/api/v1/users";
+export const GENRE_URL = "http://localhost:3000/api/api/v1/genre";
+export const MOVIE_URL = "http://localhost:3000/api/api/v1/movies";
+export const UPLOAD_URL = "http://localhost:3000/api/api/v1/upload";
