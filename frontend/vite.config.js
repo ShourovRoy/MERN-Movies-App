6e59c57c85c1cb11423f3dfd7cdff02882ff89d0
Shourov Roy <shourovroy2389@gmail.com>
2024-05-06 12:36:41 +0600

    vite config and dockerfile udpate

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,16 +2,32 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     proxy: {
+//       "/api/": "http://localhost:3000",
+//       "/uploads/": "http://localhost:3000",
+//     },
+//   },
+  
+//  preview: {
+//   host: true,
+//   port: 8000
+//  }
+// });
+
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      "/api/": "http://backend:3000",
-      "/uploads/": "http://backend:3000",
+      "/api/": "http://localhost:3000",
+      "/uploads/": "http://localhost:3000",
     },
-  },
- preview: {
-  host: true,
-  port: 8000
- }
-});
+   watch: {
+    usePolling: true,
+   },
+   host: true, // Here
+   strictPort: true,
+   port: 8000, 
+ }})
